16a7f74c529ad22d734ee9a1291de2b7195397d2
netabirami <net.abirami@gmail.com>
2024-01-08 22:21:33 +0100

    ecommerce application

---
+++ src/main/java/com/zalando/ECommerceManagement/ECommerceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zalando.ECommerceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceManagementApplication.class, args);
+	}
+
+}
