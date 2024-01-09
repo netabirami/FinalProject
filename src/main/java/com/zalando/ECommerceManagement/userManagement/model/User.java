295792c85db41d214648401efde962f7d15a5ca0
netabirami <net.abirami@gmail.com>
2024-01-09 12:57:51 +0100

    readme, Get API, and Post API, commit

---
+++ src/main/java/com/zalando/ECommerceManagement/userManagement/model/User.java
@@ -0,0 +1,23 @@
+package com.zalando.ECommerceManagement.userManagement.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull(message = "The Name should not be Null.")
+    private String name;
+    @NotNull(message = "The Email should not be Null")
+    private String email;
+    @NotNull(message = "The Password should not Null")
+    private String password;
+}
