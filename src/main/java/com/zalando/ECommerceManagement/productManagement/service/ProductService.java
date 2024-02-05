0a4e845f58bf692c75ad22ec2cfa31a3b34cc16d
netabirami <net.abirami@gmail.com>
2024-02-05 17:57:23 +0100

    CartItem put  and unit test fot get all Cart Item

--- src/main/java/com/zalando/ECommerceManagement/productManagement/service/ProductService.java
+++ src/main/java/com/zalando/ECommerceManagement/productManagement/service/ProductService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private  ProductRepository productRepository ;
+    private final ProductRepository productRepository ;
 
     @Autowired
     public ProductService(ProductRepository productRepository) {
